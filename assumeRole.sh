7598ff9b968e8ddc244013bff3828459cb513384
Thien <11982736+tpask@users.noreply.github.com>
2021-07-15 15:44:47 -0700

    Updated to make it easier to follow:

--- assumeRole.sh
+++ assumeRole.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 # Usage:
 #
-# assumeRole ${TARGET_ROLE_ARN} ${TARGET_PROFILE}
+# assumeRole ${targetAccountId} ${targetRole}
 # aws sts get-caller-identity --profile ${TARGET_PROFILE}
 
-assumeRole() {
-  ROLE_ARN=$1
-  OUTPUT_PROFILE=$2
+targetAccountId=$1
+targetRole=$2
 
-  echo "Assuming role $ROLE_ARN"
+assumeRole() {
+  targetRole=$2
+  targetAccountId=$1
+  targetRoleArn=arn:aws:iam::${targetAccountId}:role/${targetRole}
+  targetProfile="${targetAccountId}-${targetRole}"
+
   sts=$(aws sts assume-role \
-    --role-arn "$ROLE_ARN" \
-    --role-session-name "$OUTPUT_PROFILE" \
+    --role-arn "${targetRoleArn}" \
+    --role-session-name "${targetRole}" \
     --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
     --output text)
-  echo "Converting sts to array"
-  sts=($sts)
-  echo "AWS_ACCESS_KEY_ID is ${sts[0]}"
-  aws configure set aws_access_key_id ${sts[0]} --profile $OUTPUT_PROFILE
-  aws configure set aws_secret_access_key ${sts[1]} --profile $OUTPUT_PROFILE
-  aws configure set aws_session_token ${sts[2]} --profile $OUTPUT_PROFILE
-  echo "credentials stored in the profile named $OUTPUT_PROFILE"
+  if [ $? -eq 0 ]; then
+    sts=($sts)
+    echo "AWS_ACCESS_KEY_ID is ${sts[0]}"
+    aws configure set aws_access_key_id ${sts[0]} --profile ${targetProfile}
+    aws configure set aws_secret_access_key ${sts[1]} --profile ${targetProfile}
+    aws configure set aws_session_token ${sts[2]} --profile ${targetProfile}
+    echo "credentials stored in the profile: ${targetProfile}"
+  else
+   echo "Error: $?"
+   echo "Not able to assume Role"
+  fi
 }
-
-TARGET_ROLE_ARN=$1
-TARGET_PROFILE=$2
-assumeRole ${TARGET_ROLE_ARN} ${TARGET_PROFILE}
-aws sts get-caller-identity --profile ${TARGET_PROFILE}
+assumeRole ${targetAccountId} ${targetRole}
