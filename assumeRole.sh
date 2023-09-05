a38406e2f7e2dd87c48eef507869d9b296cbe66d
Thien <11982736+tpask@users.noreply.github.com>
2023-09-05 14:36:32 -0700

    Update assumeRole.sh

--- assumeRole.sh
+++ assumeRole.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 # Usage:
 #
-# assumeRole ${targetAccountId} ${targetRole}
+# assumeRole ${sourceProfile} ${targetAccountId} ${targetRole}
 # aws sts get-caller-identity --profile ${TARGET_PROFILE}
 
-targetAccountId=$1
-targetRole=$2
+sourceProfile=$1
+targetAccountId=$2
+targetRole=$3
 
 assumeRole() {
-  targetRole=$2
-  targetAccountId=$1
   targetRoleArn=arn:aws:iam::${targetAccountId}:role/${targetRole}
   targetProfile="${targetAccountId}-${targetRole}"
 
-  sts=$(aws sts assume-role --role-arn "${targetRoleArn}" --role-session-name "${targetRole}" \
+  sts=$(aws sts assume-role --profile ${sourceProfile} --role-arn "${targetRoleArn}" --role-session-name "${targetRole}" \
     --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text)
-    
+
   if [ $? -eq 0 ]; then
     sts=($sts)
     echo "AWS_ACCESS_KEY_ID is ${sts[0]}"
@@ -28,4 +27,4 @@ assumeRole() {
    echo "Not able to assume Role"
   fi
 }
-assumeRole ${targetAccountId} ${targetRole}
+assumeRole ${sourceProfile} ${targetAccountId} ${targetRole}
