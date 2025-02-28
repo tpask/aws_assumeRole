a42e65cecda5e501079d337fd0bff4e6102bf0d0
Thien <11982736+tpask@users.noreply.github.com>
2025-02-27 22:54:33 -0800

    Update assumeRoleSimple.sh

--- assumeRoleSimple.sh
+++ assumeRoleSimple.sh
@@ -1,4 +1,4 @@
-roleArn=$1  #e.g. arn:aws:iam::123456789012:role/targetRoleName
+roleArn=arn:aws:iam::$1:role/$2 #e.g. arn:aws:iam::123456789012:role/targetRoleName
 
 # ****
 # This script assumes that you already authenticated to your source account either via keys
@@ -11,7 +11,7 @@ roleArn=$1  #e.g. arn:aws:iam::123456789012:role/targetRoleName
 export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
   $(aws sts assume-role --role-arn ${roleArn} \
   --role-session-name mysession \
-  --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]"
+  --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
   --output text))
 
 # verify that you authenticated correctly to the desired destination role:
